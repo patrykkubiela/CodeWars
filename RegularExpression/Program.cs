6cf4fcd5bf36e63f6463fd7eae56a024d00d4be1
pkubiela <pkubiela@gmail.com>
2018-12-08 21:40:29 +0100

    final version of exercise with regular expression

--- RegularExpression/Program.cs
+++ RegularExpression/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
 
 namespace RegularExpression
 {
-    class Program
+    public static class Program
     {
+
+        
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var number = CountSmileys(new string[] { ";]", ":[", ";*", ":$", ";-D" });
+            System.Console.WriteLine($"Faces number: {number}");
+            System.Console.ReadLine();
+        }
+
+        public static int CountSmileys(string[] smileys) 
+        {
+            return smileys.Count(x => _regex.IsMatch(x));
         }
     }
 }
